de12bb3dff13164a3ed3df33da7a207d270ee03f
hsw6281913 <hsw628@gmail.com>
2025-11-25 16:35:19 +0900

    saucedemo 장바구니 TEST 추가

--- tests/study.test.js
+++ tests/study.test.js
@@ -6,16 +6,21 @@ test.describe('sacudemo 로그인&로그아웃',() =>{
         await page.goto('/');
         console.log('>>> 메인 페이지 이동')
     });
-
-
-    //로그인 성공 테스트
-    test('로그인 성공', async ({page}) => {
+    //공통 로그인 함수 만들기
+    async function loginstanduser(page) {
         await page.getByPlaceholder('Username').fill('standard_user');
         await page.getByPlaceholder('Password').fill('secret_sauce');
         await page.getByRole('button', {name : 'Login'}).click();
+        console.log('>> 공통 로그인 성공');
+    };
+
+    //로그인 성공 테스트
+    test('로그인 성공', async ({page}) => {
+        await loginstanduser(page);
         await expect(page).toHaveURL(/inventory\.html/);
         await expect(page.getByText('Products')).toBeVisible();
     });
+
     //로그인 실패 케이스 (비번 입력 실패)
     test('로그인 실패', async ({page}) => {
         await page.getByPlaceholder('Username').fill('standard_user');
@@ -27,9 +32,7 @@ test.describe('sacudemo 로그인&로그아웃',() =>{
     });
     // 로그아웃 확인
     test('로그아웃 확인', async ({page}) => {
-        await page.getByPlaceholder('Username').fill('standard_user');
-        await page.getByPlaceholder('Password').fill('secret_sauce');
-        await page.getByRole('button', {name : 'Login'}).click();
+        await loginstanduser(page);
         await expect(page).toHaveURL(/inventory\.html/);
         console.log('>>로그인 성공')
         await page.getByRole('button', { name: 'Open Menu' }).click();
@@ -39,4 +42,13 @@ test.describe('sacudemo 로그인&로그아웃',() =>{
         await expect(page).toHaveURL('https://www.saucedemo.com/');
         console.log('>>> 로그아웃 성공');
     });
+    // 장바구니 추가
+    test('장바구니 추가', async ({page}) => {
+        await loginstanduser(page);
+        await expect(page).toHaveURL(/inventory\.html/);
+        console.log ('>> 로그인 성공');
+        await page.getByRole('button', { name: 'Add to cart'}).first().click();
+        await expect(page.getByRole('button',{name : 'Remove'}).first()).toBeVisible();
+        console.log ('>> 장바구니 담기 성공');
+    })
 })
