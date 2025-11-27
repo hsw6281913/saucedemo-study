93f74e37838486ae43c4703310e6d98ffe055bd6
hsw6281913 <hsw628@gmail.com>
2025-11-27 18:31:23 +0900

    saucedemo 자동화 구조 바꾸기

---
+++ tests/helpers/auth.js
@@ -0,0 +1,9 @@
+import { expect } from "@playwright/test";
+
+export async function loginstanduser(page) {
+    await page.goto('/');
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page).toHaveURL(/inventory\.html/);
+}
--- tests/study.test.js
+++ tests/study.test.js
@@ -1,4 +1,6 @@
 import { test, expect } from '@playwright/test';
+import { loginstanduser } from './helpers/auth';
+
 
 test.describe('sacudemo 로그인&로그아웃',() =>{
     //공통 메인 페이지 이동
@@ -6,14 +8,7 @@ test.describe('sacudemo 로그인&로그아웃',() =>{
         await page.goto('/');
         console.log('>>> 메인 페이지 이동')
     });
-    //공통 로그인 함수 만들기
-    async function loginstanduser(page) {
-        await page.getByPlaceholder('Username').fill('standard_user');
-        await page.getByPlaceholder('Password').fill('secret_sauce');
-        await page.getByRole('button', {name : 'Login'}).click();
-        console.log('>> 공통 로그인 성공');
-    };
-
+    
     //로그인 성공 테스트
     test('로그인 성공', async ({page}) => {
         await loginstanduser(page);
@@ -30,11 +25,17 @@ test.describe('sacudemo 로그인&로그아웃',() =>{
         await expect(page).not.toHaveURL(/inventory\.html/);
         console.log('>> 로그인 실패');
     });
-    // 로그아웃 확인
-    test('로그아웃 확인', async ({page}) => {
+})
+
+
+test.describe('로그아웃/장바구니 추가 기능 묶기',() => {
+    test.beforeEach(async ({page}) => {
         await loginstanduser(page);
         await expect(page).toHaveURL(/inventory\.html/);
         console.log('>>로그인 성공')
+    });
+    // 로그아웃 확인
+    test('로그아웃 확인', async ({page}) => {
         await page.getByRole('button', { name: 'Open Menu' }).click();
         console.log('>>> 메뉴 선택 완료');
         await page.getByText('Logout').click();
@@ -44,11 +45,21 @@ test.describe('sacudemo 로그인&로그아웃',() =>{
     });
     // 장바구니 추가
     test('장바구니 추가', async ({page}) => {
-        await loginstanduser(page);
-        await expect(page).toHaveURL(/inventory\.html/);
-        console.log ('>> 로그인 성공');
         await page.getByRole('button', { name: 'Add to cart'}).first().click();
         await expect(page.getByRole('button',{name : 'Remove'}).first()).toBeVisible();
         console.log ('>> 장바구니 담기 성공');
-    })
-})
+    });
+    // 장바구니 숫자 1 확인
+    test ('장바구니 숫자 1 업데이트 확인', async ({page}) => {
+        //장바구니 담기
+        await page.getByRole('button', { name: 'Add to cart'}).first().click();
+        await expect(page.getByRole('button',{name : 'Remove'}).first()).toBeVisible();
+
+        //장바구니 숫자 1 업데이트
+        const cartBadge =  page.locator('.shopping_cart_badge');
+        await expect(cartBadge).toHaveText('1');
+        console.log('>> 장바구니 숫자 1 확인');
+    });
+});
+
+    
